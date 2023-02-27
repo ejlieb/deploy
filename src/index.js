35d956bec02f78ee8e826e861466914e4e6d5fa5
unknown <coryonronda@gmail.com>
2023-02-27 14:58:40 +0900

    add route app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BroswerRouter, Routes, Route } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BroswerRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+      </Routes>
+    </BroswerRouter>
   </React.StrictMode>
 );
 
