30a14086a271f89032552445ee6b9097506d820a
unknown <coryonronda@gmail.com>
2023-02-28 12:59:44 +0900

    eslint error fix

--- src/pages/Cosmos.jsx
+++ src/pages/Cosmos.jsx
@@ -1,30 +1,30 @@
-import { Graph, GraphConfigInterface } from "@cosmograph/cosmos";
+import { Graph } from "@cosmograph/cosmos";
 import { useEffect, useRef } from "react";
 import data from '../data/data.json'
 
 function Cosmos() {
     const target = useRef();
 
-    const config = {
-        backgroundColor: "#151515",
-        nodeSize: 2,
-        width: 100,
-        nodeColor: "#4B5BBF",
-        nodeGreyoutOpacity: 0.1,
-        linkWidth: 0.1,
-        linkColor: "#5F74C2",
-        linkArrows: false,
-        linkGreyoutOpacity: 0,
-        simulation: {
-          linkDistance: 1000,
-          linkSpring: 2,
-          repulsion: 0.2,
-          gravity: 0.1,
-          decay: 100
-        },
-    }
 
     useEffect(() => {
+        const config = {
+            backgroundColor: "#151515",
+            nodeSize: 2,
+            width: 100,
+            nodeColor: "#4B5BBF",
+            nodeGreyoutOpacity: 0.1,
+            linkWidth: 0.1,
+            linkColor: "#5F74C2",
+            linkArrows: false,
+            linkGreyoutOpacity: 0,
+            simulation: {
+              linkDistance: 1000,
+              linkSpring: 2,
+              repulsion: 0.2,
+              gravity: 0.1,
+              decay: 100
+            },
+        }
         console.log(target.current);
         console.log(data.nodes)
         console.log(data.edges)
