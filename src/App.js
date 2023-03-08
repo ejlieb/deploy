229bf7fe81089080576e13564e220025803a567b
unknown <coryonronda@gmail.com>
2023-03-08 17:48:01 +0900

    add search / seperate container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import Chart from './components/Chart';
 
 function App() {
+
+  const [searchVal, setSearchVal] = useState();
+
+  const searchValue = (e) => {
+    console.log(e.target.value)
+    setSearchVal(e.target.value)
+  }
+
+  const search = () => {
+    console.log(searchVal)
+  }
   return (
     <div className="App">
-      <div className="sigma-container">
-        <Chart/>
+      <div className="search-bar">
+          <input type="text" onChange={searchValue}></input>
+          <button onClick={search}>submit</button>
       </div>
+      <div className='container'>
+        <div id="sigma-container">
+          <Chart/>
+        </div>
+        <div id="sigma-container">
+          <Chart/>
+        </div>
+      </div>
+      
     </div>
   );
 }
