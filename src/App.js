56ede5f7f0cc3000fb23c67581566b1b62585126
unknown <coryonronda@gmail.com>
2023-03-09 13:58:23 +0900

    add lighthouse ci

--- src/App.js
+++ src/App.js
@@ -5,24 +5,36 @@ import Chart from './components/Chart';
 function App() {
 
   const [searchVal, setSearchVal] = useState();
+  const [searchQuery, setSearchQuery] = useState();
 
   const searchValue = (e) => {
-    console.log(e.target.value)
-    setSearchVal(e.target.value)
+    console.log(e.target.value);
+    setSearchVal(e.target.value);
   }
 
   const search = () => {
-    console.log(searchVal)
+    setSearchQuery(searchVal);
   }
+
   return (
     <div className="App">
-      <div className="search-bar">
-          <input type="text" onChange={searchValue}></input>
-          <button onClick={search}>submit</button>
+      <div className='filter-container'>
+        <div className="filter-bar">
+            <label><input type="checkbox" value="CC301E"/> CC301E</label>
+            <label><input type="checkbox" value="D44028"/> D44028</label>
+            <label><input type="checkbox" value="B30000"/> B30000</label>
+            <label><input type="checkbox" value="B70805"/> B70805</label>
+            <label><input type="checkbox" value="BB100A"/> BB100A</label>
+            <label><input type="checkbox" value="D8482D"/> D8482D</label>
+        </div>
+        <div className="search-bar">
+            <input type="text" onChange={searchValue}></input>
+            <button onClick={search}>submit</button>
+        </div>
       </div>
       <div className='container'>
         <div id="sigma-container">
-          <Chart/>
+          <Chart searchquery={searchQuery}/>
         </div>
         <div id="sigma-container">
           <Chart/>
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,20 +5,17 @@ import { Graph } from "graphology";
 import { SigmaContainer, useLoadGraph, useRegisterEvents, useSigma } from "@react-sigma/core";
 import data from "../data/data.json"
 
-const LoadGraphWithHook = () => {
+const LoadGraphWithHook = (props) => {
   const [toggle, setToggle] = useState(false);
   const [target, setTarget] = useState();
   const MyGraph = () => {
-    const loadGraph = useLoadGraph();
+  const loadGraph = useLoadGraph();
 
-    useEffect(() => {
-      const graph = new Graph();
-      //   graph.addNode("A", { x: 0, y: 0, label: "Node A", size: 10 });
-      //   graph.addNode("B", { x: 1, y: 1, label: "Node B", size: 10 });
-      //   graph.addEdgeWithKey("rel1", "A", "B", { label: "REL_1" });
-      graph.import(data)
-        loadGraph(graph);
-      }, [loadGraph]);
+  useEffect(() => {
+    const graph = new Graph();  
+    graph.import(data);
+      loadGraph(graph);
+    }, [loadGraph]);
 
     return null;
   };
