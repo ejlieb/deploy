8f6bcaac3138536c6c1f125c34310c82b378ca26
unknown <coryonronda@gmail.com>
2023-03-10 09:47:48 +0900

    change config lhci file

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,10 +3,11 @@ module.exports = {
       collect: {
         staticDistDir: "./build",
         // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
-        settings: {
-            chromeFlags: "--no-sandbox --headless"
-        },
+        // settings: {
+        //     chromeFlags: "--no-sandbox --headless"
+        // },
         numberOfRuns: 5,
+        url:["http://localhost/", "http://localhost/sigma", "http://localhost/cosmos"]
           },
       upload: {
         target: 'temporary-public-storage',
