967f635da03a70ac594729e3d2e78a8dfa1cdc71
unknown <coryonronda@gmail.com>
2023-03-10 14:12:53 +0900

    lhci fix

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    ci: {
-      collect: {
-        staticDistDir: "./build",
-        // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
-        // settings: {
-        //     chromeFlags: "--no-sandbox --headless"
-        // },
-        numberOfRuns: 5,
-        url:["http://localhost/", "http://localhost/sigma", "http://localhost/cosmos"]
-          },
-      upload: {
-        target: 'temporary-public-storage',
+  ci: {
+    collect: {
+      // staticDistDir: "./build",
+      url: ["http://localhost:3000/", "http://localhost:3000/sigma", "http://localhost:3000/cosmos" ],
+      startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
+      settings: {
+          chromeFlags: "--no-sandbox --headless"
       },
+      numberOfRuns: 5,
+        },
+    upload: {
+      target: 'temporary-public-storage',
     },
-  };
+  },
+};
